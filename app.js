4512a5e85383687cd79a6c375eaa55d282f67ac2
Leo Santillan <lcskode@gmail.com>
2018-10-31 13:42:15 +0800

    Added body-parser package and modified app.js to extract data from a form, added data to friendsArr array.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
 
+// extracts data added to req.body object through a form 
+var bodyParser = require("body-parser");
+
+// instruct app to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
+// create dummy array of friends
+var friendsArr = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 // ============================================================================
 // ROUTES
@@ -15,16 +23,25 @@ app.get("/", function(req, res){
 
 // friends ROUTE - displays list of friends
 app.get("/friends", function(req, res){
-  // create dummy array of friends
-  var friendsArr = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+  
   // pass friends array 
   res.render("friends", {friends: friendsArr});
 });
 
 // post ROUTE - adds or  sends data to be added or to be used somehow on the server side 
 app.post("/addFriend", function(req, res){
+  // req.body - an object that contains all data from the request body
+  // displays new friend added from friends.ejs via the form element 
+  // console.log(req.body.newFriend);
+
+  var newFriend = req.body.newFriend;
+  friendsArr.push(newFriend);
+
   // Test
-  res.send("You have reached the POST ROUTE!");
+  // res.send("You have reached the POST ROUTE!");
+  
+  // redirects to friends ROUTE after adding new friend to friendsArr array
+  res.redirect("/friends");
 });
 
 // ============================================================================
