4772bd1feb1e5256c30c1c6cf7d95f2681cccc89
Leo Santillan <lcskode@gmail.com>
2018-10-31 12:41:51 +0800

    Added friends.ejs and some dummy array to display friends list.

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ var app = express();
 app.set("view engine", "ejs");
 
 
+// ============================================================================
+// ROUTES
+// ============================================================================
+
+// root ROUTE
 app.get("/", function(req, res){
   res.render("home");
 });
 
+// friends ROUTE - displays list of friends
+app.get("/friends", function(req, res){
+  // create dummy array of friends
+  var friendsArr = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+  // pass friends array 
+  res.render("friends", {friends: friendsArr});
+});
 
+// ============================================================================
 // SERVER
+// ============================================================================
 app.listen(3000, function(){
   console.log("Server started...");
 });
